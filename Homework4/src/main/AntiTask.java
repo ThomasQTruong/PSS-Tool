5433a43583949096b426f9f00e7c7e13c29cc7be
Thomas Truong <TruongThomasQ@gmail.com>
2023-05-07 02:24:18 -0700

    Set up the basic file.

--- Homework4/src/main/AntiTask.java
+++ Homework4/src/main/AntiTask.java
@@ -1,5 +1,41 @@
 package main;
 
-public class AntiTask {
-    
+/**
+ * Placeholder javadoc.
+ */
+public class AntiTask implements Task {
+  // Common setter methods.
+  public void setName(String newName) {
+  }
+
+  public void setStartTime(int newTime) {
+  }
+
+  public void setDuration(int newDuration) {
+  }
+
+  public void setDate(int newDate) {
+  }
+
+
+  // Common getter methods.
+  public String getName() {
+    return name;
+  }
+
+  public int getType() {
+    return type;
+  }
+
+  public int getStartTime() {
+    return startTime;
+  }
+
+  public int getDuration() {
+    return duration;
+  }
+
+  public int getStartDate() {
+    return startDate;
+  }
 }
