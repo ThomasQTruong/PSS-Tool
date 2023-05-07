ff44e565f581eaa9007376b3f287b2be3b0974c6
Richard Medina <medinarichard996@gmail.com>
2023-05-07 14:46:07 -0700

    Add functions and setter to schedule.

--- Homework4/src/main/Schedule.java
+++ Homework4/src/main/Schedule.java
@@ -7,4 +7,46 @@ import java.util.ArrayList;
  */
 public class Schedule {
   private ArrayList<Task> listOfTasks;
+
+  //initalize new schedule class
+  public Schedule() {
+    listOfTasks = new ArrayList<>();
+  }
+
+  //function for adding task to list
+  public void addTask(Task task) {
+    if (reportOverlap(task)) {
+        System.out.println("Overlap detected. Task cannot be added.");
+        return;
+    }
+    listOfTasks.add(task);
+  }
+
+  //function for removing task from list
+  public void removeTask(Task task) {
+      listOfTasks.remove(task);
+  }
+
+  //function for checking overlaps of tasks
+  private boolean reportOverlap(Task newTask) {
+    for (Task task : listOfTasks) {
+        if (task.getStartDate() == newTask.getStartDate()) {
+            if (task.getStartTime() + task.getDuration() <= newTask.getStartTime() + newTask.getDuration()) {
+                return true; // Overlap detected
+            }
+        }
+    }
+    return false; // No overlap found
+  }
+
+  //function for generating schedule
+  private void generateSchedule() {
+
+  }
+
+  //setter
+  public void setList() {
+
+  }
+
 }
