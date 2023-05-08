d0fabd34017f95ac45c07870eaeb926c645be7ab
Thomas Truong <TruongThomasQ@gmail.com>
2023-05-08 04:00:46 -0700

    Testing console utility class.

--- Homework4/src/main/PSS.java
+++ Homework4/src/main/PSS.java
@@ -7,6 +7,7 @@ public class PSS {
   private Schedule schedule;
 
   public static void main(String[] args) {
-    System.out.println("TESTING");
+    int test = ConsoleInput.getIntRange("Enter a number 10 - 20.", 10, 20);
+    System.out.println(test);
   }
 }
