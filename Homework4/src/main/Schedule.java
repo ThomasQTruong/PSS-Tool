0c141b8da976767b6465df6d874135114db28be1
Thomas Truong <TruongThomasQ@gmail.com>
2023-05-08 02:40:32 -0700

    Imported Google's GSON library.

--- Homework4/src/main/Schedule.java
+++ Homework4/src/main/Schedule.java
@@ -1,5 +1,6 @@
 package main;
 
+import com.google.gson.JsonObject;
 import java.util.ArrayList;
 
 /**
@@ -8,45 +9,49 @@ import java.util.ArrayList;
 public class Schedule {
   private ArrayList<Task> listOfTasks;
 
-  //initalize new schedule class
+  // initalize new schedule class
   public Schedule() {
     listOfTasks = new ArrayList<>();
   }
 
-  //function for adding task to list
+  // function for adding task to list
   public void addTask(Task task) {
     if (reportOverlap(task)) {
-        System.out.println("Overlap detected. Task cannot be added.");
-        return;
+      System.out.println("Overlap detected. Task cannot be added.");
+      return;
     }
     listOfTasks.add(task);
   }
 
-  //function for removing task from list
+  // function for removing task from list
   public void removeTask(Task task) {
-      listOfTasks.remove(task);
+    listOfTasks.remove(task);
   }
 
-  //function for checking overlaps of tasks
+  // function for checking overlaps of tasks
   private boolean reportOverlap(Task newTask) {
     for (Task task : listOfTasks) {
-        if (task.getStartDate() == newTask.getStartDate()) {
-            if (task.getStartTime() + task.getDuration() <= newTask.getStartTime() + newTask.getDuration()) {
-                return true; // Overlap detected
-            }
+      if (task.getStartDate() == newTask.getStartDate()) {
+        if (task.getStartTime() + task.getDuration()
+            <= newTask.getStartTime() + newTask.getDuration()) {
+          return true;  // Overlap detected
         }
+      }
     }
-    return false; // No overlap found
+    return false;  // No overlap found
   }
 
-  //function for generating schedule
+  // function for generating schedule
   private void generateSchedule() {
 
   }
 
-  //setter
-  public void setList() {
-
+  /**
+   * Loads tasks from a JSON file.
+   * 
+   * @param 
+   */
+  public boolean loadFromJson(JsonObject json) {
+    return true;
   }
-
 }
