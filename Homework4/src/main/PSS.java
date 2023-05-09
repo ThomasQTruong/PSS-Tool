4594f71e051a248f4e9d23dc2c39fdc8d8bc1372
Thomas Truong <TruongThomasQ@gmail.com>
2023-05-08 22:30:49 -0700

    Started on the main program.

--- Homework4/src/main/PSS.java
+++ Homework4/src/main/PSS.java
@@ -9,6 +9,111 @@ public class PSS {
   private Schedule schedule;
 
   public static void main(String[] args) {
+    int userInput;
+
+    do {
+      // Print and get user's operation choice.
+      printMenu();
+      userInput = ConsoleInput.getIntRange("Please choose an operation.", 0, 9);
+      System.out.println();
+
+      // Check operation choice.
+      switch (userInput) {
+        case 0:
+          // Exit program.
+          System.out.println("Good bye!");
+          break;
+        case 1:
+          // Create a task.
+          break;
+        case 2:
+          // View a task.
+          break;
+        case 3:
+          // Delete a task.
+          break;
+        case 4:
+          // Edit a task.
+          break;
+        case 5:
+          // Write the schedule to a file.
+          break;
+        case 6:
+          // Read the schedule from a file.
+          break;
+        case 7:
+          // View or write the schedule for one day.
+          break;
+        case 8:
+          // View or write the schedule for one week.
+          break;
+        case 9:
+          // View or write the schedule for one month.
+          break;
+      }
+    } while (userInput != 0);
+  }
+
+  /**
+   * Prints the operations menu.
+   */
+  private static void printMenu() {
+    System.out.println("=Operations Menu=");
+    System.out.println("[1] Create a task");
+    System.out.println("[2] View a task");
+    System.out.println("[3] Delete a task");
+    System.out.println("[4] Edit a task");
+    System.out.println("[5] Write the schedule to a file");
+    System.out.println("[6] Read the schedule from a file");
+    System.out.println("[7] View or write the schedule for one day");
+    System.out.println("[8] View or write the schedule for one week");
+    System.out.println("[9] View or write the schedule for one month");
+    System.out.println("[0] Exit Program");
+  }
+
+  // Option 1.
+  private static boolean createTask() {
+    return true;
+  }
+
+  // Option 2.
+  private static boolean viewTask() {
+    return true;
+  }
+
+  // Option 3.
+  private static boolean deleteTask() {
+    return true;
+  }
+
+  // Option 4.
+  private static boolean editTask() {
+    return true;
+  }
+
+  // Option 5.
+  private static boolean saveToFile() {
+    return true;
+  }
+
+  // Option 6.
+  private static boolean loadFromFile() {
+    return true;
+  }
+
+  // Option 7 (???).
+  private static boolean viewOrWriteForOneDay() {
+    return true;
+  }
+
+  // Option 8 (???).
+  private static boolean viewOrWriteForOneWeek() {
+    return true;
+  }
+
+  // Option 9 (???).
+  private static boolean viewOrWriteForOneMonth() {
+    return true;
   }
 
   // assuming the user is given option to input recurring,
@@ -53,6 +158,6 @@ public class PSS {
 
   // get schedule from Schedule class -- Brian Kang
   public ArrayList<Task> getSchedule() {
-    return schedule.generateSchedule();
+    return schedule.getTasks();
   }
 }
