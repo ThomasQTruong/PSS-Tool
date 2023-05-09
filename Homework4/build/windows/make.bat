7a3b107886369d1857d4be4fc34003d593872f2a
Thomas Truong <TruongThomasQ@gmail.com>
2023-05-09 04:27:18 -0700

    Updated scripts to support new test package.

--- Homework4/build/windows/make.bat
+++ Homework4/build/windows/make.bat
@@ -1,2 +1,2 @@
 javac -d ../../bin/ ../../src/main/*.java -cp "../../lib/*;../../bin/"
-javac -d ../../bin/test/ ../../src/test/*.java -cp "../../lib/*;../../bin/"
+javac -d ../../bin/ ../../src/test/*.java -cp "../../lib/*;../../bin/"
