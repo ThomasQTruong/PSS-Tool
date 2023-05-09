18788e5a688d2ed07817e1c3c14788683b57755d
Thomas Truong <TruongThomasQ@gmail.com>
2023-05-09 04:27:54 -0700

    Added a test file for PSS class.

---
+++ Homework4/src/test/TestPSS.java
@@ -0,0 +1,12 @@
+package test;
+
+import main.PSS;
+
+/**
+ * Used for unit testing the PSS class.
+ */
+public class TestPSS {
+  public static void main(String[] args) {
+    System.out.println(PSS.roundMinutesToNearest15(10f));
+  }
+}
