52bb9add87cbbb5e663a92504fa944aa00834204
Thomas Truong <TruongThomasQ@gmail.com>
2023-05-09 04:28:32 -0700

    Converted to a test package.

--- Homework4/src/test/TestSchedule.java
+++ Homework4/src/test/TestSchedule.java
@@ -1,8 +1,11 @@
+package test;
+
 import main.Schedule;
 
 /**
  * Used for unit testing the Schedule class.
  */
 public class TestSchedule {
-
+  public static void main(String[] args) {
+  }
 }
--- Homework4/src/test/TestTask.java
+++ Homework4/src/test/TestTask.java
@@ -1,9 +1,12 @@
+package test;
+
 import main.Task;
 
 /**
  * Used for unit testing the Task class.
  */
 public class TestTask {
-
+  public static void main(String[] args) {
+  }
 }
  
