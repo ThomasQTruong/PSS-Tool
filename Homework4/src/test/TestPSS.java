af5da567a3bf52d6489d3dcc6d974dec4ca281ce
ThomasQTruong <TruongThomasQ@gmail.com>
2023-05-09 18:01:00 -0700

    Added tests for roundMinutesToNearest15()

--- Homework4/src/test/TestPSS.java
+++ Homework4/src/test/TestPSS.java
@@ -7,6 +7,26 @@ import main.PSS;
  */
 public class TestPSS {
   public static void main(String[] args) {
-    System.out.println(PSS.roundMinutesToNearest15(10f));
+    // Test roundMinutesToNearest15()
+    System.out.println("Testing: Test_roundMinutesToNearest15()");
+    float[] case1 =     {22.74f, 22.75f, 20.01f, 13.76f, 12.51f, 5.12f};
+    float[] expected1 = {22.75f, 22.75f, 20.25f, 14.00f, 12.75f, 5.25f};
+    if (Test_roundMinutesToNearest15(case1, expected1)) {
+      System.out.println("roundMinutesToNearest15() was successful.");
+    }
+  }
+
+  // Tests roundMinutesToNearest15()
+  public static boolean Test_roundMinutesToNearest15(float[] cases, float[] expected) {
+    boolean success = true;
+    for (int i = 0; i < cases.length; ++i) {
+      float result = PSS.roundMinutesToNearest15(cases[i]);
+      if (result != expected[i]) {
+        System.out.printf("roundMinutesToNearest15(%f): expected: %f, got %f.\n", cases[i], expected[i], result);
+        success = false;
+      }
+    }
+
+    return success;
   }
 }
