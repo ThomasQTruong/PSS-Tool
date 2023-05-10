0a127723fa1d4af71f756c4749ca3aa28f4fe12a
Thomas Truong <TruongThomasQ@gmail.com>
2023-05-10 04:03:52 -0700

    reportOverlap() isn't correct; needs to be fixed.

--- Homework4/src/main/Schedule.java
+++ Homework4/src/main/Schedule.java
@@ -10,12 +10,14 @@ import java.util.List;
 public class Schedule {
   private ArrayList<Task> listOfTasks;
 
+
   /**
    * Constructor: initalizes new schedule object.
    */
   public Schedule() {
     listOfTasks = new ArrayList<>();
   }
+
 
   /**
    * Retrieves the list of tasks.
@@ -26,6 +28,7 @@ public class Schedule {
     return listOfTasks;
   }
 
+
   /**
    * Adds a task to listOfTasks if it is not overlapping.
    *
@@ -33,6 +36,10 @@ public class Schedule {
    * @return boolean - whether the task was successfully added or not.
    */
   public boolean addTask(Task task) {
+    // Task name already exists.
+    if (containsName(task.getName())) {
+      return false;
+    }
     // Given task is overlapping with another task.
     if (reportOverlap(task)) {
       return false;
@@ -42,6 +49,7 @@ public class Schedule {
     return true;
   }
 
+
   /**
    * Deletes a task from listOfTask if it exists.
    *
@@ -50,13 +58,31 @@ public class Schedule {
    */
   public boolean deleteTask(Task task) {
     // Task does not exist.
-    if (task == null && !listOfTasks.contains(task)) {
+    if (task == null || !listOfTasks.contains(task)) {
       return false;
     }
     // Task exists, remove from listOfTasks.
     listOfTasks.remove(task);
     return true;
   }
+
+
+  /**
+   * Checks if a task in listOfTasks contains the name.
+   *
+   * @param name - the name to check for.
+   * @return boolean - whether a task has the name or not.
+   */
+  private boolean containsName(String name) {
+    for (Task task : listOfTasks) {
+      if (task.getName().equals(name)) {
+        return true;
+      }
+    }
+
+    return false;
+  }
+
 
   /**
    * Reports whether newTask overlaps with any other tasks.
@@ -65,16 +91,20 @@ public class Schedule {
    * @return boolean - whether newTask overlaps or not.
    */
   private boolean reportOverlap(Task newTask) {
+    // For every task.
     for (Task task : listOfTasks) {
+      // If the start date of the new task exists.
       if (task.getStartDate() == newTask.getStartDate()) {
-        if (task.getStartTime() + task.getDuration()
-            <= newTask.getStartTime() + newTask.getDuration()) {
+        // Task start time + duration is less than new task's start time.
+        if (task.getStartTime() + task.getDuration() <= newTask.getStartTime()) {
+          System.out.printf("%f | %f <= %f | %f\n", task.getStartTime(), task.getDuration(), newTask.getStartTime(), newTask.getDuration());
           return true;  // Overlap detected
         }
       }
     }
     return false;  // No overlap found
   }
+
 
   // apply antiTask to listOfTask to remove recurring task from schedule
   // when antiTask is entered through PSS class. --Brian Kang
@@ -100,7 +130,13 @@ public class Schedule {
     }
   }
 
-  // remove task by its name -- Brian Kang
+
+  /**
+   * Deletes a task with a specific name.
+   *
+   * @param taskName - the name of the task to delete.
+   * @return boolean - whether the deletion was successful or not.
+   */
   public boolean deleteTaskByName(String taskName) {
     Task taskToDelete = null;
     for (Task task : listOfTasks) {
@@ -109,13 +145,9 @@ public class Schedule {
         break;
       }
     }
-    if (taskToDelete != null) {
-      deleteTask(taskToDelete);
-      return true;
-    } else {
-      return false;
-    }
+    return deleteTask(taskToDelete);
   }
+
 
   /**
    * Loads tasks from a JSON file.
