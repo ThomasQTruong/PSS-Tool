404c322c6596fe549e0c5d1bcc3b225b71fa6e85
Thomas Truong <TruongThomasQ@gmail.com>
2023-05-10 23:31:40 -0700

    Added javadoc comments for every method.

--- Homework4/src/main/Task.java
+++ Homework4/src/main/Task.java
@@ -10,32 +10,97 @@ public interface Task {
   public static final int ANTI_TASK = 3;
 
 
-  // Common setter methods.
+  // [SETTERS]
+  /**
+   * Sets the name of the task with a new one.
+   *
+   * @param name - the new name.
+   */
   public void setName(String name);
 
+  /**
+   * Sets the type of the task with a new one.
+   *
+   * @param type - the new type.
+   */
   public void setType(String type);
 
+  /**
+   * Sets the start time of the task with a new one.
+   *
+   * @param startTime - the new start time.
+   */
   public void setStartTime(float startTime);
 
+  /**
+   * Sets the duration of the task with a new one.
+   *
+   * @param duration - the new duration.
+   */
   public void setDuration(float duration);
 
+  /**
+   * Sets the start date of the task with a new one.
+   *
+   * @param startDate - the new start date.
+   */
   public void setStartDate(int startDate);
 
 
-  // Common getter methods.
+  // [GETTERS]
+  /**
+   * Retrieves the identity.
+   *
+   * @return int - the identity.
+   */
   public int getIdentity();
 
+  /**
+   * Retrieves the available task types.
+   *
+   * @return String[] - the task types.
+   */
   public String[] getTypes();
 
+  /**
+   * Retrieves the length of the available task types.
+   *
+   * @return int - the length of the available task types.
+   */
   public int getTypeLength();
 
+  /**
+   * Retrieves the name.
+   *
+   * @return String - the name.
+   */
   public String getName();
 
+  /**
+   * Retrieves the type.
+   *
+   * @return String - the type.
+   */
   public String getType();
 
+  /**
+   * Retrieves the start time.
+   *
+   * @return float - the start time.
+   */
   public float getStartTime();
 
+  /**
+   * Retrieves the duration.
+   *
+   * @return float - the duration.
+   */
   public float getDuration();
 
+  /**
+   * Retrieves the start date.
+   *
+   * @return int - the start date.
+   */
   public int getStartDate();
 }
