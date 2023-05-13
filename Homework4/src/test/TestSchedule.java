20acccbc8119f233cfe90717ab169303b7be19a6
Thomas Truong <TruongThomasQ@gmail.com>
2023-05-13 00:28:44 -0700

    Tested Schedule::getTasksInRange().

--- Homework4/src/test/TestSchedule.java
+++ Homework4/src/test/TestSchedule.java
@@ -1,13 +1,32 @@
 package test;
 
+import java.util.ArrayList;
 import main.Schedule;
-import main.TransientTask;
 import main.Task;
+import main.TransientTask;
+
 /**
  * Used for unit testing the Schedule class.
  */
 public class TestSchedule {
   public static void main(String[] args) {
+    // Most of testing for schedule is done in the main program.
+    
+    // Test_Richard().
+    System.out.println("Testing Richard's cases.");
+    Test_Richard();
+    System.out.println("=Test_Richard() END=\n");
+
+
+    // Test_getTasksInRange().
+    System.out.println("Testing Test_getTasksInRange().");
+    if (Test_getTasksInRange()) {
+      System.out.println("Test_getTasksInRange() passed all cases.");
+    }
+  }
+
+  // Richard's test.
+  public static void Test_Richard() {
     //initialize task and schedule
     Schedule schedule = new Schedule();
     TransientTask transientTask = new TransientTask();
@@ -25,26 +44,63 @@ public class TestSchedule {
     transientTask2.setStartTime(12.35f);
     transientTask2.setDuration(14.35f);
 
-    //test task is added to schedule
-    System.out.println("Testing adding a new task to schedule");
+    // test task is added to schedule 
     boolean added = schedule.addTask(transientTask);
-    System.out.println("Expected true got " + added);
+    if (!added) {
+      System.out.println("Testing adding a new task to schedule");
+      System.out.println("Expected true got " + added);
+    }
 
     //test to see if same task can be added again
-    System.out.println("Testing adding same task to schedule");
     boolean addedAgain = schedule.addTask(transientTask);
-    System.out.println("Expected false got " + addedAgain);
+    if (addedAgain) {
+      System.out.println("Testing adding same task to schedule");
+      System.out.println("Expected false got " + addedAgain);
+    }
 
     //test to see if schedule can remove task
-    System.out.println("Testing removing of a task");
     boolean removed = schedule.deleteTask(transientTask);
-    System.out.println("Expected true got " + removed);
+    if (!removed) {
+      System.out.println("Testing removing of a task");
+      System.out.println("Expected true got " + removed);
+    }
 
     //test to see if a schedule contains a task
     schedule.addTask(transientTask);
     schedule.addTask(transientTask2);
-    System.out.println("Testing schedule contains a task");
     Task returnedTask = schedule.getByName(transientTask2.getName());
-    System.out.println("Expected task to equal task searched. Result: " + (returnedTask.equals(transientTask2)));
+    boolean equalResult = returnedTask.equals(transientTask2);
+    if (!equalResult) {
+      System.out.println("Testing schedule contains a task");
+      System.out.println("Expected task to equal task searched. Result: "
+                          + (returnedTask.equals(transientTask2)));
+    }
+  }
+
+  public static boolean Test_getTasksInRange() {
+    Schedule schedule = new Schedule();
+    schedule.loadFromJson("../../test/getDatesInRange.json");
+    boolean fullSuccess = true;
+
+    ArrayList<Task> tasksInRange1 = schedule.getTasksInRange(20230514, 20230519);
+    if (tasksInRange1.size() != 5) {
+      System.out.println("tasksInRange1: expected 5, but got " + tasksInRange1.size());
+      fullSuccess = false;
+    }
+
+    ArrayList<Task> tasksInRange2 = schedule.getTasksInRange(20230514, 20230515);
+    if (tasksInRange2.size() != 4) {
+      System.out.println("tasksInRange2: expected 4, but got " + tasksInRange2.size());
+      fullSuccess = false;
+    }
+
+    // Testing 2023 to 2025 (includes everything).
+    ArrayList<Task> tasksInRange3 = schedule.getTasksInRange(20230514, 20250515);
+    if (tasksInRange3.size() != 6) {
+      System.out.println("tasksInRange3: expected 6, but got " + tasksInRange3.size());
+      fullSuccess = false;
+    }
+
+    return fullSuccess;
   }
 }
