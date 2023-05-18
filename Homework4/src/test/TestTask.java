87d49bcc23c2dfdf300a4710209c68bcaf0183db
Tophuu <89433013+Pnutru@users.noreply.github.com>
2023-05-17 21:20:31 -0700

    added testing for linking process

--- Homework4/src/test/TestTask.java
+++ Homework4/src/test/TestTask.java
@@ -1,12 +1,53 @@
 package test;
 
 import main.Task;
+import main.Schedule;
+import main.AntiTask;
+import main.RecurringTask;
 
 /**
  * Used for unit testing the Task class.
  */
 public class TestTask {
   public static void main(String[] args) {
+
+    testLinking();
+  
+    if(testLinking()){
+      System.out.println("testLinking() is successful!");
+    }
+
+    
   }
+
+  public static boolean testLinking(){
+    //create recurring task
+    //create antitask
+    //adds them to list of schedule
+    //uses getAntiTask / getRecurringTask to find their respective linked task/
+
+    boolean success = true;
+    Schedule schedule = new Schedule();
+
+    Task running = schedule.createTask(1, "Running" , "Visit", 1.25f, 1.25f , 20230609 , 20230709 , 4);
+    Task noRunning = schedule.createTask(0, "I Hate Running " , "Visit" , 1.25f, 1.25f , 20230609, 20230609, 1);
+
+    schedule.addTask(running);
+    schedule.addTask(noRunning);
+
+    if(((RecurringTask) running).getAntiTask() == null || ((AntiTask) noRunning).getRecurringTask() == null){
+      success = false;
+    }
+
+    schedule.deleteTask(noRunning);
+
+
+    if(((RecurringTask) running).getAntiTask() != null){
+      success = false;
+    }
+
+    return success;
+  }
+
 }
  
